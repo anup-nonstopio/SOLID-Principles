644129c7c05996de762ec2c81735fc3738542364
Anup Gupta <anup.gupta@nonstopio.com>
2024-07-01 18:18:37 +0530

    SOLID: after applying DIP

--- 5: DIP/dip.ts
+++ 5: DIP/dip.ts
@@ -1,34 +1,73 @@
+// DIP: Dependency Inversion Principle
+
+interface Repository {
+    save(): void;
+}
+
+interface Service {
+    save(): void;
+}
+
 class UserController {
-    userService: UserService;
+    service: Service;
 
     // dependency injection
-    constructor(userService: UserService) {
-        this.userService = userService;
+    constructor(service: Service) { // depends on abstraction not on concrete classes
+        this.service = service;
     }
 
     save() {
-        this.userService.save();
+        this.service.save();
     }
 }
 
 class UserService {
     // dependency injection
-    constructor(private userRepository: UserRepository) {}
+    constructor(private repository: Repository) {} // depends on abstraction
 
     save() {
-        this.userRepository.save();
+        this.repository.save();
     }
 }
 
-class UserRepository {
+class UserRepository implements Repository {
     save() {
         console.log('save user in the database');
     }
 }
 
-// violation of DIP
-const userRepository = new UserRepository();
-const userService = new UserService(userRepository);
+class UserPostgresRepository implements Repository {
+    save() {
+        console.log('save user in the postgres database');
+    }
+}
+
+class UserMongoRepository implements Repository {
+    save() {
+        console.log('save user in the mongo database');
+    }
+}
+
+class UserFakeRepository implements Repository {
+    save() {
+        console.log('save user in the fake database');
+    }
+}
+
+class ModernUserService implements Service {
+    constructor(private repository: Repository) {}
+
+    save() {
+        this.repository.save();
+    }
+}
+
+const postgresRepository = new UserPostgresRepository(); // can be changed to UserRepository
+const mongoRepository = new UserMongoRepository(); // can be changed to UserRepository
+const fakeRepository = new UserFakeRepository(); // can be changed to UserRepository
+
+//const userService = new UserService(fakeRepository); 
+const userService = new ModernUserService(fakeRepository); // can be changed to UserService
 const userController = new UserController(userService);
 
 userController.save();
