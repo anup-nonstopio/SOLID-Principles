6bb883f7519d9ef374a02886164f03a867ce5a73
Anup Gupta <anup.gupta@nonstopio.com>
2024-07-01 15:21:52 +0530

    SOLID: after applying SRP

--- 1: SRP/srp.ts
+++ 1: SRP/srp.ts
@@ -1,5 +1,8 @@
 // Single responsibility principle
+import { Invoice } from './Invoice';
 import { Product, Order } from './Order';
+import { PaymentProcessor } from './PaymentProcessor';
+import { PricingCaluculator } from './PricingCalculator';
 
 const product1 = new Product('1', 'Laptop', 500);
 const product2 = new Product('2', 'Iphone', 1000);
@@ -8,5 +11,11 @@ const order = new Order();
 order.addProduct(product1);
 order.addProduct(product2);
 
-order.generateInvoice();
-order.processPayment();
+const pricingCaluclator = new PricingCaluculator();
+const total = pricingCaluclator.calculatePricing(order.getProducts());
+
+const invoice = new Invoice();
+invoice.generateInvoice(order.getProducts(), total);
+
+const paymentProcessor = new PaymentProcessor();
+paymentProcessor.processPayment(order);
