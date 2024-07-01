46c6f3c9c6d7cc9e330132f585179ae1074e678f
Anup Gupta <anup.gupta@nonstopio.com>
2024-07-01 15:50:23 +0530

    SOLID: after applying OCP

--- 2: OCP/ocp.ts
+++ 2: OCP/ocp.ts
@@ -1,17 +1,35 @@
 // Open-close principle
+
+interface IPaymentProcessor {
+    processPayment(amount: number): void;
+}
+
 class PaymentProcessor {
-    processPayment(amount: number, paymentType: string) {
-        if(paymentType === 'creditcard') {
-            console.log(`Processing credit card payment of $${amount}`);
-        }
-        else if(paymentType === 'paypal') {
-            console.log(`Processing PayPal payment of $${amount}`);
-        }
-        else if(paymentType === 'googlepay') {
-            console.log(`Processing Google Pay payment of $${amount}`);
-        }
-        else {
-            console.log(`Unknown payment type: ${paymentType}`);
-        }
+    processor: IPaymentProcessor;
+
+    constructor(paymentProcessor: IPaymentProcessor) {
+        this.processor = paymentProcessor;
+    }
+
+    processPayment(amount: number) {
+        this.processor.processPayment(amount);
     }
 }
+
+class CreditCardProcessor implements IPaymentProcessor {
+    processPayment(amount: number) {
+        console.log(`Processing credit card payment of ${amount}`);
+    }
+}
+
+class PaypalProcessor implements IPaymentProcessor {
+    processPayment(amount: number) {
+        console.log(`Processing paypal payment of ${amount}`);
+    }
+}
+
+const creditCardProcessor = new CreditCardProcessor();
+const paypalProcessor = new PaypalProcessor();
+
+const processor = new PaymentProcessor(paypalProcessor);
+processor.processPayment(100);
